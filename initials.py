35f6fad27e5f043d3549687c6df84a4a0fdd5961
drew-harsh <197388455+drew-harsh@users.noreply.github.com>
2026-05-31 23:12:17 +0000

    Add files via upload

--- initials.py
+++ initials.py
@@ -1,7 +1,31 @@
+#File: initials.py
+#Description: Print out my initials.
+#Assignment Number: 1
+#
+#Name: Andrews Appiah
+#Student ID: 2425401189
+#Email: andrewsappiah022@gmail.com
+#Grader: Augustus Buckman
+#
+#On my honor, Andrews Appiah, this programming assignment is my own work
+#and I have not provided this code to any other student.
 
 def main():
-  pass #code goes here
+    #Print initials DAA
+    print()
+    print("...DAA")
+    print()
 
-
+    print("...DDDDDDDD................AAA.................AAA..........")
+    print("...DD......DDD............AA..AA..............AA..AA........")
+    print("...DD......DDD...........AA....AA............AA....AA.......")
+    print("...DD......DDD..........AA......AA..........AA......AA......")
+    print("...DD......DDD..........AAAAAAAAAA..........AAAAAAAAAA......")
+    print("...DD......DDD..........AA......AA..........AA......AA......")
+    print("...DD......DDD..........AA......AA..........AA......AA......")
+    print("...DD......DDD..........AA......AA..........AA......AA......")
+    print("...DD......DDD....**....AA......AA....**....AA......AA....**")
+    print("...DDDDDDDD.......**....AA......AA....**....AA......AA....**")
+    print()
 
 main()
